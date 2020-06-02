a833f66ff473ecdc1e20905f7f4ef39194ca683e
Michael Odusanya <modusanya12@gmail.com>
2020-06-02 19:09:24 -0400

    Decement complete

---
+++ src/Decrement.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Decrement extends Component{
+    constructor(props){
+        super(props);
+        this.state = {number: props.start};
+    }
+
+
+    handleDecrement = (state) =>{
+        if(state.number===0){
+            return alert("CAN'T GO LOWER THAN ZERO MY GUY!!!");
+        }
+        this.setState({number: state.number - 1});
+    };
+
+    render(){
+        return(
+            <div>
+                {this.state.number}{" "}
+                <button onClick = {()=>this.handleDecrement(this.state)}>
+                    decrement 
+                </button>
+            </div>
+        );
+    }
+}
+
+Decrement.propTypes = {
+    start: PropTypes.number.isRequired,
+};
+
+export default Decrement;
